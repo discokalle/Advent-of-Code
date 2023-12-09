7d7c48ca653d0850f45764e97be908a7cc0fb26e
kalle <karlduckert@gmail.com>
2023-12-09 18:24:24 +0100

    day 7 Kompletos

--- 7/day7_2.py
+++ 7/day7_2.py
@@ -1,10 +1,17 @@
 with open ('input.txt', 'r') as file:
     lines = file.readlines()
 
-def get_hand_type(s):
-    occurrences = {char: 0 for char in set(s)} #Dict with values 0 for chars (ignores repeats)
+def count_occurrences(s):
+    occurrences = {char: 0 for char in set(s)}  #Dict with values 0 for chars (ignores repeats)
     for c in s:
         occurrences[c] = occurrences.get(c, 0) + 1 
+    return occurrences
+
+def get_hand_type(s):
+    occurrences = count_occurrences(s)
+    most_occurring = max(occurrences, key = lambda x: occurrences[x] if x != 'J' else 0) #find most occurring character that isn't J
+    s = s.replace('J', most_occurring)
+    occurrences = count_occurrences(s)
     occurrences = (sorted(occurrences.values(), reverse=True)) #list sorted from most to least occurring. Example: 32T3K will yield [2, 1, 1, 1]
     
     match occurrences[0]:               #Check first card (and sometimes second) to determine the type of hand.
@@ -24,7 +31,7 @@ def get_hand_type(s):
             return 0 
 
 def my_sort(hand):
-    strength = "123456789TJQKA"
+    strength = "J123456789TQKA"
     return(get_hand_type(hand), #Sort by hand type first. If they are the same, sort by hand[0]...
            strength.index(hand[0]),
            strength.index(hand[1]),
