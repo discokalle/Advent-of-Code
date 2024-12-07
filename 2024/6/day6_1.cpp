fbd701567e2e4cc792c793f82c9241fc6ad55330
kalle <karlduckert@gmail.com>
2024-12-07 12:50:18 +0100

    day6 part 1. Wtf is part 2 though; might look later...

---
+++ 2024/6/day6_1.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+
+int main() {
+    std::ifstream file{"input.txt"};
+    std::vector<std::string> grid{};
+    std::string line{};
+    
+    while (std::getline(file, line)) {
+        grid.push_back(line);
+    }
+    
+    int res{};
+    std::pair<int, int> curr{52, 36}; //Start pos: 6,4 for ex; 52,36 for input
+    int dir{}; //0-N, 1-E, 2-S, 3-W
+    std::set<std::pair<int, int>> visited{};
+    while(1) {
+        if(visited.insert(curr).second) ++res; //set.insert(x).second returns false if x already in set
+
+        std::pair<int, int> next{curr.first + (dir == 2) - (dir == 0),  // N: -1, S: +1
+                                curr.second + (dir == 1) - (dir == 3)}; // E: +1, W: -1
+
+        if ((next.first == -1) || (next.first == grid.size()) //out of bounds; done.
+        ||(next.second == -1) || (next.second == grid.size())) {
+            break;
+        }
+        
+        switch(grid[next.first][next.second]) {
+            case '#':
+                dir = (dir + 1) % 4; //rotate
+                break;
+            case '.':
+            case '^':
+                curr = next;
+                break;
+        }
+    }
+    std::cout << res << std::endl;
+    return 0;
+}
